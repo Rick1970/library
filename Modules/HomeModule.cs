ba6650487e8416b5fae0b2118ee6a6d7e7107756
Andrew Niekamp, Rick Thornbrugh and shokouh Farvid <andrew+rick+shokouh@>
2016-08-04 12:26:26 -0700

    add nancy view and forms to add author and book, and link in join table

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -18,6 +18,26 @@ namespace Library
         return View["index.cshtml", model];
       };
 
+      Get["/librarian"] = _ => {
+        return View["librarian.cshtml"];
+      };
+
+      Post["/success"] = _ => {
+        Book newBook = new Book(Request.Form["title-input"]);
+        newBook.Save();
+
+        Author newAuthor = new Author(Request.Form["author-input"]);
+        newAuthor.Save();
+
+        newBook.AddAuthor(newAuthor);
+
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        model.Add("addedBook", newBook);
+        model.Add("addedAuthor", newAuthor);
+
+        return View["success.cshtml", model];
+      };
+
       Post["/results"] = _ => {
         string input = Request.Form["search-input"];
         Dictionary<string, object> model = new Dictionary<string, object>();
